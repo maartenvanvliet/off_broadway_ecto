199f63a185f135256adc0875bfe6ddb25ff65053
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2023-03-30 15:28:14 +0200

    Update behaviour

--- lib/off_broadway_ecto/client.ex
+++ lib/off_broadway_ecto/client.ex
@@ -1,5 +1,5 @@
 defmodule OffBroadwayEcto.Client do
   @callback receive_messages(demand :: integer, opts :: Keyword.t()) :: term
-  @callback handle_failed(schemas :: list(term)) :: :ok
-  @callback handle_successful(schemas :: list(term)) :: :ok
+  @callback handle_failed(schemas :: list(term), client_opts :: Keyword.t()) :: :ok
+  @callback handle_successful(schemas :: list(term), client_opts :: Keyword.t()) :: :ok
 end
